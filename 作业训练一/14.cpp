1e445a1f2e417429921de248c2e5c8908f4f20b5
吴俊德 <95957417+WuJunDehuda@users.noreply.github.com>
2022-06-22 15:29:19 +0800

    Update 14.cpp
    修改版

--- 作业训练一/14.cpp
+++ 作业训练一/14.cpp
@@ -10,32 +10,36 @@ int dat2num(string s){
     if(s=="saturday") return 5;
     if(s=="sunday")  return 6;
 }
-int main(){
-    int T,L,R;
-    string S,E;
-    int s,e;
-    cin>>T;
-    for(int i=0;i<T;i++){
-        cin>>S>>E;
+int main() {
+    int T, L, R;
+    string S, E;
+    int s, e;
+    cin >> T;
+    int ans;
+    for (int i = 0; i < T; i++) {
+        cin >> S >> E;
         s = dat2num(S);
         e = dat2num(E);
-        cin>>L>>R;
-        if(e-s>0&&(e-s+1)<L){
-            cout<<"impossible";
+        cin >> L >> R;
+        if (e - s > 0 && (e - s + 1) < L) {
+            cout << "impossible"<<endl;
+            break;
+        } else if (e - s < 0 && (e - s + 8) < L) {
+            cout << "impossible"<<endl;
+            break;
         }
-        else if(e-s<0&&(e-s+8)<L){
-            cout<<"impossible";
+        if (R - L > 7) {
+            cout << "many" << endl;
+            break;
         }
-        if(R-L>7) cout<<"many"<<endl;
-        else {
-            int tmp = L/7;
-            int s1 = (s+L-1)%7;
-            int s2 = (s+R-1)%7;
-            if(e>=min(s1,s2)&&e<=max(s1,s2)){
-                if(s<e) cout<<tmp*7+e-s+1<<endl;
-                else cout<<tmp*7+7+s-e+1<<endl;
+        int temp = abs(e - s) + 1;
+        if (s > e) temp += 7;
+        for (int j = L; j <= R; j++) {
+            if (j % 7 == temp % 7) {
+                ans = j;
             }
         }
+        cout<<ans<<endl;
     }
+    return 0;
 }
-
